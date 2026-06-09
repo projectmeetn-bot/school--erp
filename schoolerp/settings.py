a565f887ca5bb49471ff0caf1a372358ef6d0bc4
Meet <projectmeetn@gmail.com>
2026-06-09 10:24:46 +0530

    Fix  render hosts

--- schoolerp/settings.py
+++ schoolerp/settings.py
@@ -26,11 +26,8 @@ SECRET_KEY = 'django-insecure-!7=b#mua@occq#bsc$7wyrtzj1k8dr0%x2#h%=k=vsn%rd&#1p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'school-erp-ns3g.onrender.com',
-    'localhost',
-    '127.0.0.1'
-    ]
+ALLOWED_HOSTS = ['*']
+
 CSRF_TRUSTED_ORIGINS = [
     'https://school-erp-ns3g.onrender.com'
 ]
