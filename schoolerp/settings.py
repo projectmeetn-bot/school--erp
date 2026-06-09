3239c0db3e72321d4b0bf80067eda4d3e8fa81aa
Meet <projectmeetn@gmail.com>
2026-06-09 10:19:05 +0530

    Fix  allowed hosts

--- schoolerp/settings.py
+++ schoolerp/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-!7=b#mua@occq#bsc$7wyrtzj1k8dr0%x2#h%=k=vsn%rd&#1p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'school-erp-ns3g.onrender.com/',
+    'localhost',
+    '127.0.0.1']
 
 
 # Application definition
