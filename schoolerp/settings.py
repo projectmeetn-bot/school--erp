adfc2b324f25dbac6f154631d5138863ef3c35bc
Meet <projectmeetn@gmail.com>
2026-06-09 11:05:27 +0530

    Use PostgreSQL

--- schoolerp/settings.py
+++ schoolerp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -91,10 +92,9 @@ WSGI_APPLICATION = 'schoolerp.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get()
+    )
 }
 
 
